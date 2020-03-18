ea5a02eb899c2ff1d36eb967e9686afb866cbfc3
Shealtiel elizer <62135926+shealthi@users.noreply.github.com>
2020-03-18 17:07:23 +0530

    Add files via upload

---
+++ MOTOR/MOTOR.ino
@@ -0,0 +1,48 @@
+
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(10, 11, 4, 5, 6, 7);
+const int pin1=3;
+const int pin2=2;
+int pin1state=0;
+int pin2state=0;
+//*******************************************************
+void setup()
+{
+ lcd.begin(16, 2);
+ pinMode(pin1,OUTPUT);
+ pinMode(pin2,OUTPUT);
+ pin1state=LOW;
+ pin2state=LOW;
+ digitalWrite(pin1,pin1state);
+ digitalWrite(pin2,pin2state);
+ delay(1000);
+
+  lcd.print("Rotation of motor");  
+  lcd.setCursor(0, 1);
+  lcd.print("clk wise and anticlk wise");  
+ delay(4000);
+ 
+}
+//**********************************************************
+void loop()
+{
+  lcd.clear();
+ pin1state=LOW;
+ pin2state=HIGH;
+digitalWrite(pin1,pin1state);
+ digitalWrite(pin2,pin2state);
+ lcd.print("anticlk wise"); // MOTOR WILL ROTATE ANTICLK WISE TILL 5 SEC
+    delay(5000);
+    lcd.clear();
+
+     pin1state=HIGH;
+  pin2state=LOW;
+digitalWrite(pin1,pin1state);
+ digitalWrite(pin2,pin2state);
+ lcd.print("clk wise");// MOTOR WILL ROTATE CLK WISE TILL 5 SEC
+    delay(5000);
+     }
+//*************************************************************
+
+
+
